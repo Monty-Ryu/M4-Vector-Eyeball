94415f239201664a97a4691f33734fb3ad4fbfbe
Monty <montather.al-hassy@mohawkcollege.ca>
2026-02-24 17:13:22 -0500

    Add
    - Drawing eyeball

---
+++ M4-Vecctor-Eyeball/Game.cs
@@ -0,0 +1,53 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetTitle("Eye follow vector");
+            Window.SetSize(400, 400);
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+            Window.ClearBackground(Color.OffWhite);
+            drawEyeball(Window.Size / 2);
+        }
+
+        void drawEyeball(Vector2 positin)
+        {
+            // Cornea
+            Draw.LineSize = 1;
+            Draw.LineColor = Color.Black;
+            Draw.FillColor = Color.White;
+            Draw.Circle(positin, 50);
+
+
+            //Iris
+            Draw.FillColor = Color.Gray;
+            Draw.Circle(positin, 35);
+
+            //Pupil
+            Draw.FillColor = Color.Black;
+            Draw.Circle(positin, 20);
+        }
+    }
+
+}
